4dc87aeee30e937ac4827005d1574c64f36c981d
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2022-08-09 17:11:18 -0500

    calcKL(): allow multiple thetas

--- R/calc_kl_functions.r
+++ R/calc_kl_functions.r
@@ -6,22 +6,23 @@ NULL
 #' \code{\link{calcKL}} is a function to calculate Kullback-Leibler information.
 #'
 #' @param object an \code{\linkS4class{item_pool}} object.
-#' @param theta the theta value to use. This must be a single value.
+#' @param theta theta values to use.
 #' @param theta_width the theta width to use to define the range of integration. The range is \code{c(theta - theta_width, theta + theta_width)}.
 #' @param quadrature_unit the quadrature spacing to use for integration. Smaller numbers yield more accurate values.
 #'
 #' @return
 #' \describe{
-#'   \item{\code{\linkS4class{item_pool}} object:}{\code{\link{calcKL}} returns a length-\emph{ni} vector of information values.}
+#'   \item{\code{\linkS4class{item_pool}} object:}{\code{\link{calcKL}} returns a (\emph{nx}, \emph{ni}) matrix of information values.}
 #' }
 #' \describe{
 #'   \item{\emph{notations}}{\itemize{
+#'     \item{\emph{nx} denotes the number of theta values in the 'theta' argument.}
 #'     \item{\emph{ni} denotes the number of items in the \code{\linkS4class{item_pool}} object.}
 #'   }}
 #' }
 #'
 #' @examples
-#' info_pool <- calcKL(itempool_science, 0, 1, 10)
+#' info_pool <- calcKL(itempool_science, seq(-3, 3, 1), 1, 10)
 #'
 #' @template 1pl-ref
 #' @template 2pl-ref
@@ -62,22 +63,29 @@ setMethod(
     NCAT <- object@NCAT
     info <- numeric(ni)
 
-    theta_q <- seq(theta - theta_width, theta + theta_width, by = quadrature_unit)
+    n_theta <- nrow(theta)
+    info <- matrix(0, n_theta, ni)
 
-    for (i in 1:ni) {
-      ncat_thisitem <- NCAT[i]
-      prob_origin <- calcProb(object@parms[[i]], theta)
-      prob_area   <- calcProb(object@parms[[i]], theta_q)
-      info_thisitem <- 0
-      for (k in 1:ncat_thisitem) {
-        info_thisitem <- info_thisitem +
+    for (theta_idx in 1:n_theta) {
+
+      this_theta <- theta[theta_idx]
+      theta_q <- seq(this_theta - theta_width, this_theta + theta_width, by = quadrature_unit)
+
+      for (i in 1:ni) {
+        ncat_thisitem <- NCAT[i]
+        prob_origin <- calcProb(object@parms[[i]], this_theta)
+        prob_area   <- calcProb(object@parms[[i]], theta_q)
+        info_thisitem <- 0
+        for (k in 1:ncat_thisitem) {
+          info_thisitem <- info_thisitem +
           sum(prob_origin[k] * log(prob_origin[k] / prob_area[, k]))
+        }
+        info[theta_idx, i] <- info[theta_idx, i] + info_thisitem
       }
-      info[i] <- info[i] + info_thisitem
+
     }
 
     return(info)
 
   }
-
 )
